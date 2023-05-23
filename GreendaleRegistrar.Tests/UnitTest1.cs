61e9d585546f390a11b8191ee40b4987893ecea2
Justin Shaffer <justinbshaffer91@gmail.com>
2023-05-22 20:03:23 -0700

    Set up initial project structure with solution, web project, and test project.

---
+++ GreendaleRegistrar.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GreendaleRegistrar.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ GreendaleRegistrar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GreendaleRegistrar.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
